87a72ada2ce92e8472844a46d88ce6f65c937f69
quiorte <g.mouthuy@gmail.com>
2022-06-22 18:04:25 +0200

    master

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+
+
+struct bird {
+    name: String,
+    age: u32,
+    size: String
+}
+
+impl bird {
+    fn print_name(&self) {
+        println!("{}", self.name);
+    }
+}
+
+fn first <T> (slice: &[T]) -> &T {
+    &slice[0]
+}
+
+impl dinosaurs for bird {
+    fn can_fly(&self) -> bool {
+        true
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let birdy = bird { name: "birdy".to_string(), age: 25, size: "small".to_string() };
+    birdy.print_name();
+    print!("{}", birdy.age);
+
+}
+trait dinosaurs {
+    fn can_kill_you(&self) -> bool {
+        true
+    }
+    fn can_fly(&self) -> bool;
+
 }
